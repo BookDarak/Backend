acdde523fac70c252690776bd0b2287b44af78b6
hs12 <77109954+hyun2371@users.noreply.github.com>
2023-05-16 20:49:54 +0900

    [Feat] 프로젝트 생성

---
+++ bookdarak/src/main/java/com/bookjeok/bookdarak/BookdarakApplication.java
@@ -0,0 +1,13 @@
+package com.bookjeok.bookdarak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookdarakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookdarakApplication.class, args);
+	}
+
+}
