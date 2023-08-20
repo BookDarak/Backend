38974a657aa3a539113864eed5ddc75224ce6e19
hs12 <loveanna1228@gmail.com>
2023-08-20 22:07:34 +0900

    [Feat] 단일 리뷰, 캘린더 조회 DTO 필드 추가

--- bookdarak/src/main/java/com/bookjeok/bookdarak/dto/review/ReviewRes.java
+++ bookdarak/src/main/java/com/bookjeok/bookdarak/dto/review/ReviewRes.java
@@ -1,10 +1,8 @@
 package com.bookjeok.bookdarak.dto.review;
 
 import com.bookjeok.bookdarak.domain.Review;
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -18,6 +16,10 @@ public class ReviewRes {
 
     @Getter @AllArgsConstructor
     public static class GetReviewRes{
+        private String name;
+        private String author;
+        private String bookImgUrl;
+
         private BigDecimal rating;
         private String content;
         private String phrase;
@@ -28,6 +30,9 @@ public class ReviewRes {
 
 
         public GetReviewRes(Review review) {
+            this.name = review.getBook().getName();
+            this.author = review.getBook().getAuthor();
+            this.bookImgUrl = review.getBook().getImgUrl();
             this.rating = review.getRating();
             this.content = review.getContent();
             this.phrase = review.getPhrase();
@@ -40,11 +45,17 @@ public class ReviewRes {
 
     @Getter
     public static class Calendar{
-        private Long reviewId;
-        private LocalDate startDate;
-        private LocalDate endDate;
+        private final String name;
+        private final String author;
+        private final String bookImgUrl;
+        private final Long reviewId;
+        private final LocalDate startDate;
+        private final LocalDate endDate;
 
         public Calendar(Review review) {
+            this.name = review.getBook().getName();
+            this.author = review.getBook().getAuthor();
+            this.bookImgUrl = review.getBook().getImgUrl();
             this.reviewId = review.getId();
             this.startDate = review.getStartDate();
             this.endDate = review.getEndDate();
