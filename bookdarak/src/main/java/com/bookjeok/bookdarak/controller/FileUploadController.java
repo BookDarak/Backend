cf3389ed8dcc3c8f9a503c449608290cffc121e4
hs12 <loveanna1228@gmail.com>
2023-09-03 15:46:03 +0900

    [Feat] S3 설정 및 테스트

---
+++ bookdarak/src/main/java/com/bookjeok/bookdarak/controller/FileUploadController.java
@@ -0,0 +1,22 @@
+package com.bookjeok.bookdarak.controller;
+
+import com.bookjeok.bookdarak.base.BaseResponse;
+import com.bookjeok.bookdarak.service.S3UploadService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@RestController
+public class FileUploadController {
+    private final S3UploadService s3UploadService;
+
+    @PostMapping("/upload")
+    public BaseResponse<String> uploadFile(@RequestParam("image") MultipartFile multipartFile) throws IOException {
+        return new BaseResponse<>(s3UploadService.upload(multipartFile));
+    }
+}
